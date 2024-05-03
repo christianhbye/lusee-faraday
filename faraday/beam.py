212970e9cccc93d856bfc89175b26f93a2eba5f7
Christian Hellum Bye <chbye@berkeley.edu>
2024-05-03 12:03:22 -0700

    add lusee beam class

--- faraday/beam.py
+++ faraday/beam.py
@@ -1,5 +1,27 @@
 import numpy as np
 import healpy as hp
+from astropy.io import fits
+from croissant.healpix import grid2healpix
+
+
+def rotate_X2Y(beam_X, phi_axis=-1):
+    """
+    Rotate the X polarization to the Y polarization.
+
+    NOTE: This assumes that the beam is specified on a grid where
+    the spacing in phi is 1 degree.
+    """
+    return np.roll(beam_X, 90, axis=phi_axis)
+
+
+def rotate_Y2X(beam_Y, phi_axis=-1):
+    """
+    Rotate the Y polarization to the X polarization.
+
+    NOTE: This assumes that the beam is specified on a grid where
+    the spacing in phi is 1 degree.
+    """
+    return np.roll(beam_Y, -90, axis=phi_axis)
 
 
 class Beam:
@@ -17,28 +39,6 @@ class Beam:
         if self.beam_Y is not None:
             self.beam_Y = self.beam_Y[:, pix]
 
-    def rotate_X2Y(self, phi_axis=-1):
-        """
-        Rotate the X polarization to the Y polarization.
-
-        NOTE: This assumes that the beam is specified on a grid where
-        the spacing in phi is 1 degree.
-        """
-        if self.beam_X is None:
-            raise ValueError("No X beam specified.")
-        return np.roll(self.beam_X, 90, axis=phi_axis)
-
-    def rotate_Y2X(self, phi_axis=-1):
-        """
-        Rotate the Y polarization to the X polarization.
-
-        NOTE: This assumes that the beam is specified on a grid where
-        the spacing in phi is 1 degree.
-        """
-        if self.beam_Y is None:
-            raise ValueError("No Y beam specified.")
-        return np.roll(self.beam_Y, -90, axis=phi_axis)
-
 
 class ShortDipole(Beam):
     def __init__(self, nside, frequency=None):
@@ -55,4 +55,35 @@ class ShortDipole(Beam):
         E_theta = -np.cos(theta) * np.sin(phi)
         E_phi = -np.cos(phi)
         beam_Y = np.array([E_theta, E_phi]) * horizon
+        super().__init__(beam_X=beam_X, beam_Y=beam_Y, frequency=frequency)
+
+
+class LuseeBeam(Beam):
+    def __init__(self, path, frequency, nside=128):
+        with fits.open(path) as hdul:
+            E_theta = hdul["Etheta_real"].data + 1j * hdul["Etheta_imag"].data
+            E_phi = hdul["Ephi_real"].data + 1j * hdul["Ephi_imag"].data
+            ix = np.argwhere(hdul["freq"].data == frequency)[0, 0]
+            E_theta = E_theta[ix]
+            E_phi = E_phi[ix]
+        lusee_Y = np.array(
+            [E_theta.real, E_theta.imag, E_phi.real, E_phi.imag]
+        )
+        # add horizon
+        lusee_Y = np.concatenate(
+            (lusee_Y, np.zeros_like(lusee_Y)[:, :-1, :]), axis=1
+        )
+
+        lusee_X = rotate_Y2X(lusee_Y, phi_axis=-1)
+
+        # convert to healpix
+        lusee_X = grid2healpix(lusee_X, nside)
+        lusee_Y = grid2healpix(lusee_Y, nside)
+
+        beam_X = np.array(
+            [lusee_X[0] + 1j * lusee_X[1], lusee_X[2] + 1j * lusee_X[3]]
+        )
+        beam_Y = np.array(
+            [lusee_Y[0] + 1j * lusee_Y[1], lusee_Y[2] + 1j * lusee_Y[3]]
+        )
         super().__init__(beam_X=beam_X, beam_Y=beam_Y, frequency=frequency)
